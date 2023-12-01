bf9cddf36c97876dffae17cd32fdaf9318c4237f
Odonno <bottiau.david@laposte.net>
2023-12-01 17:56:06 +0100

    ⚡️ compare day1 solution with or without nom

--- src/day01/common.rs
+++ src/day01/common.rs
@@ -22,11 +22,11 @@ pub fn parse(input: &str, with_spelled_letters: bool) -> Vec<CalibrationValue> {
                 let text = &line[index..];
 
                 match parse_digit(text, with_spelled_letters) {
-                    Ok((_, digit)) => {
+                    Some(digit) => {
                         first_digit = digit;
                         break;
                     }
-                    Err(_) => continue,
+                    None => continue,
                 }
             }
 
@@ -36,11 +36,11 @@ pub fn parse(input: &str, with_spelled_letters: bool) -> Vec<CalibrationValue> {
                 let text = &line[index..];
 
                 match parse_digit(text, with_spelled_letters) {
-                    Ok((_, digit)) => {
+                    Some(digit) => {
                         last_digit = digit;
                         break;
                     }
-                    Err(_) => continue,
+                    None => continue,
                 }
             }
 
@@ -52,7 +52,54 @@ pub fn parse(input: &str, with_spelled_letters: bool) -> Vec<CalibrationValue> {
         .collect()
 }
 
-fn parse_digit(input: &str, with_spelled_letters: bool) -> IResult<&str, ParsedDigit> {
+fn parse_digit(input: &str, with_spelled_letters: bool) -> Option<ParsedDigit> {
+    const USE_NOM: bool = false;
+
+    if USE_NOM {
+        match parse_digit_nom(input, with_spelled_letters) {
+            Ok((_, digit)) => Some(digit),
+            Err(_) => None,
+        }
+    } else {
+        parse_digit_std(input, with_spelled_letters)
+    }
+}
+
+fn parse_digit_std(input: &str, with_spelled_letters: bool) -> Option<ParsedDigit> {
+    let c = input.chars().next().unwrap();
+
+    if c.is_digit(10) {
+        Some(c.to_digit(10).unwrap() as u8)
+    } else {
+        if with_spelled_letters {
+            if input.starts_with("one") {
+                Some(1)
+            } else if input.starts_with("two") {
+                Some(2)
+            } else if input.starts_with("three") {
+                Some(3)
+            } else if input.starts_with("four") {
+                Some(4)
+            } else if input.starts_with("five") {
+                Some(5)
+            } else if input.starts_with("six") {
+                Some(6)
+            } else if input.starts_with("seven") {
+                Some(7)
+            } else if input.starts_with("eight") {
+                Some(8)
+            } else if input.starts_with("nine") {
+                Some(9)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+
+fn parse_digit_nom(input: &str, with_spelled_letters: bool) -> IResult<&str, ParsedDigit> {
     let mut single_digit_parse = map(one_of("123456789"), |s| s.to_digit(10).unwrap() as u8);
 
     if with_spelled_letters {
