5dabbb7c05b469768c8d8d6e97ceaa541abf8d80
Odonno <bottiau.david@laposte.net>
2023-12-09 10:08:13 +0100

    ✨ complete day9

--- src/day09/common.rs
+++ src/day09/common.rs
@@ -1,6 +1,29 @@
-pub fn parse(input: &str) -> Vec<u32> {
+use itertools::Itertools;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{digit1, multispace1},
+    combinator::{opt, recognize},
+    multi::separated_list1,
+    sequence::preceded,
+    IResult,
+};
+
+pub type History = Vec<i32>;
+
+pub fn parse(input: &str) -> Vec<History> {
     input
         .lines()
-        .map(|line| line.parse::<u32>().unwrap())
+        .map(|line| parse_line(line).unwrap().1)
         .collect()
+}
+
+fn parse_line(input: &str) -> IResult<&str, History> {
+    let (input, values) =
+        separated_list1(multispace1, recognize(preceded(opt(tag("-")), digit1)))(input)?;
+    let values = values
+        .iter()
+        .map(|value| value.parse::<i32>().unwrap())
+        .collect_vec();
+
+    Ok((input, values))
 }
--- src/day09/part1.rs
+++ src/day09/part1.rs
@@ -1,7 +1,34 @@
+use itertools::Itertools;
+
 use super::common::*;
 
-pub fn run(input: &str) {
-    let values = parse(input);
+pub fn run(input: &str, use_sample: bool) {
+    let histories = parse(input);
 
-    println!("{:?}", values);
+    let next_history_values = histories
+        .into_iter()
+        .map(|history| next_history_value(history))
+        .collect_vec();
+
+    let value = next_history_values.iter().sum::<i32>();
+
+    if use_sample {
+        assert_eq!(value, 114);
+    } else {
+        println!("{:?}", value);
+    }
+}
+
+fn next_history_value(history: History) -> i32 {
+    let differences = history
+        .iter()
+        .tuple_windows()
+        .map(|(a, b)| b - a)
+        .collect_vec();
+
+    if differences.iter().all(|d| d == &0) {
+        history.last().unwrap() + 0
+    } else {
+        history.last().unwrap() + next_history_value(differences)
+    }
 }
