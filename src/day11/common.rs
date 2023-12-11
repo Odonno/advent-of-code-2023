02e4c6e6df23fb1c8b2ee3cbdf814a480946e204
David Bottiau <B516QT@login.axa>
2023-12-11 17:49:00 +0100

    ✨ complete day11

--- src/day11/common.rs
+++ src/day11/common.rs
@@ -1,6 +1,66 @@
-pub fn parse(input: &str) -> Vec<u32> {
-    input
-        .lines()
-        .map(|line| line.parse::<u32>().unwrap())
-        .collect()
+use itertools::Itertools;
+use std::cmp::{max, min};
+use std::collections::HashMap;
+
+pub type Position = (u8, u8);
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Pixel {
+    EmptySpace,
+    Galaxy,
+}
+
+pub type UniverseMap = HashMap<Position, Pixel>;
+
+pub fn parse(input: &str) -> UniverseMap {
+    let mut map = UniverseMap::new();
+
+    input.lines().enumerate().for_each(|(y, line)| {
+        line.chars().enumerate().for_each(|(x, char)| {
+            let pixel = match char {
+                '.' => Pixel::EmptySpace,
+                '#' => Pixel::Galaxy,
+                _ => panic!("Impossible char"),
+            };
+
+            map.insert((x as u8, y as u8), pixel);
+        });
+    });
+
+    map
+}
+
+pub fn calculate_distance_between_galaxies(
+    p1: Position,
+    p2: Position,
+    empty_rows: &Vec<u8>,
+    empty_columns: &Vec<u8>,
+    empty_space_larger_by: u64,
+) -> u64 {
+    let (x1, y1) = p1;
+    let (x2, y2) = p2;
+
+    let x = (x1 as u64).abs_diff(x2 as u64);
+    let y = (y1 as u64).abs_diff(y2 as u64);
+
+    let min_x = min(x1, x2);
+    let max_x = max(x1, x2);
+
+    let min_y = min(y1, y2);
+    let max_y = max(y1, y2);
+
+    let between_empty_rows_count = empty_rows
+        .into_iter()
+        .filter(|row| &&min_y < row && row < &&max_y)
+        .collect_vec()
+        .len() as u64;
+    let between_empty_columns_count = empty_columns
+        .into_iter()
+        .filter(|column| &&min_x < column && column < &&max_x)
+        .collect_vec()
+        .len() as u64;
+
+    let total_empty_row_or_column = between_empty_rows_count + between_empty_columns_count;
+
+    x + y + ((empty_space_larger_by - 1) * total_empty_row_or_column)
 }
--- src/day11/part2.rs
+++ src/day11/part2.rs
@@ -1,7 +1,82 @@
+use itertools::Itertools;
+
 use super::common::*;
 
-pub fn run(input: &str) {
-    let values = parse(input);
+const EMPTY_SPACE_LARGER_BY: u64 = 1000000;
 
-    println!("{:?}", values);
+pub fn run(input: &str, use_sample: bool) {
+    let map = parse(input);
+
+    let galaxy_positions = map
+        .clone()
+        .into_iter()
+        .filter_map(|(position, pixel)| match pixel {
+            Pixel::EmptySpace => None,
+            Pixel::Galaxy => Some(position),
+        })
+        .collect_vec();
+
+    let pairs = galaxy_positions
+        .into_iter()
+        .tuple_combinations::<(_, _)>()
+        .collect_vec();
+
+    let current_universe_max_x = map
+        .keys()
+        .into_iter()
+        .map(|(x, _)| x)
+        .max()
+        .unwrap()
+        .clone();
+    let current_universe_max_y = map
+        .keys()
+        .into_iter()
+        .map(|(_, y)| y)
+        .max()
+        .unwrap()
+        .clone();
+
+    let empty_rows = (0..=current_universe_max_y)
+        .into_iter()
+        .filter(|y| {
+            let mut row_pixels = map.iter().filter(|((_, pixel_y), _)| pixel_y == y);
+            let are_all_empty_space = row_pixels.all(|(_, value)| value == &Pixel::EmptySpace);
+
+            are_all_empty_space
+        })
+        .collect_vec();
+
+    let empty_columns = (0..=current_universe_max_x)
+        .into_iter()
+        .filter(|x| {
+            let mut column_pixels = map.iter().filter(|((pixel_x, _), _)| pixel_x == x);
+            let are_all_empty_space = column_pixels.all(|(_, value)| value == &Pixel::EmptySpace);
+
+            are_all_empty_space
+        })
+        .collect_vec();
+
+    let value = pairs
+        .into_iter()
+        .map(|(galaxy_position1, galaxy_position2)| {
+            calculate_distance_between_galaxies(
+                galaxy_position1,
+                galaxy_position2,
+                &empty_rows,
+                &empty_columns,
+                EMPTY_SPACE_LARGER_BY,
+            )
+        })
+        .sum::<u64>();
+
+    if use_sample {
+        if EMPTY_SPACE_LARGER_BY == 10 {
+            assert_eq!(value, 1030);
+        }
+        if EMPTY_SPACE_LARGER_BY == 100 {
+            assert_eq!(value, 8410);
+        }
+    } else {
+        println!("{:?}", value);
+    }
 }
